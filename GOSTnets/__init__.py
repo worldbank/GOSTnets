66cadd130fd7af5d2dc0f3ee03d0fdd144ed6ca1
Benjamin P. Stewart <ben.gis.stewart@gmail.com>
2019-06-19 06:01:15 -0400

    Initial Commit
    initial commit migrates the repository from GOST_Public_Goods and adds the install file

---
+++ GOSTnets/__init__.py
@@ -0,0 +1,1 @@
+from GOSTnets.GOSTnet import *
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GOSTnets",
+    version="0.1.1",
+    packages=find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=[#'docutils>=0.3',
+                      'geopandas>=0.4.0',
+                      'networkx',
+                      'numpy',
+                      'osmnx',
+                      'pandas>=0.23.5',
+                      'pyproj',
+                      'scipy',
+                      'shapely'
+                      ],
+
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        # And include any *.msg files found in the 'hello' package, too:
+    },
+
+    # metadata to display on PyPI
+    author="Benjamin P. Stewart",
+    author_email="ben.gis.stewart@gmail.com",
+    description="Networkx wrapper to simplify geospatial analysis",
+    license="PSF",
+    keywords="networkx networks OSM",
+    url="https://github.com/worldbank/GOSTnets",   # project home page, if any
+    project_urls={
+        "Bug Tracker": "https://github.com/worldbank/GOSTnets/issues",
+        "Documentation": "https://github.com/worldbank/GOSTnets",
+        "Source Code": "https://github.com/worldbank/GOSTnets",
+    }
+
+    # could also include long_description, download_url, classifiers, etc.
+)
