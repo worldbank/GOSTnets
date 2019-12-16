d544f838749bf55dd38334abd3e612b44e97f692
@tomgertin <tgertin@vt.edu>
2019-12-16 16:19:17 -0500

    Update __init__.py

--- GOSTnets/__init__.py
+++ GOSTnets/__init__.py
@@ -1,7 +1,9 @@
 from .core import *
 from .calculate_od import *
 from .conversion_utils import *
-from .load_osm import *
 from .network_clean import *
 from .optimization import *
 from .osm_parser import *
+
+# make sure gdal, geopy, and boltons are installed first
+#from .load_osm import *
