9cb6771d7152b80ecb3d3b96c417cc5c0c733013
d3netxer <tommygun320@yahoo.com>
2020-01-10 13:46:08 -0500

    fixing conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,30 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+# mock import these packages because readthedocs doesn't have them installed
+autodoc_mock_imports = [
+  'dateutil',
+  'descartes',
+  'geopandas',
+  'matplotlib',
+  'matplotlib.cm',
+  'matplotlib.collections',
+  'matplotlib.pyplot',
+  'networkx',
+  'numpy',
+  'pandas',
+  'pyproj',
+  'requests',
+  'rtree',
+  'rtree.index',
+  'scipy',
+  'scipy.spatial',
+  'shapely',
+  'shapely.geometry',
+  'shapely.ops',
+  'sklearn',
+  'sklearn.neighbors'
+]
 
 # -- Project information -----------------------------------------------------
 
@@ -26,14 +50,12 @@ author = 'Charles Fox, Keith Garrett, Benjamin Stewart, Tom Gertin, Andres Chamo
 # The full version, including alpha/beta/rc tags
 release = '0.0.2'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# extensions = ['sphinx.ext.autodoc'
-]
+# extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,7 +71,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
