2871068be775ac93d15fde77c78797c26e1a5dc8
d3netxer <tgertin@vt.edu>
2021-08-26 23:26:45 -0400

    reproject_graph doesn't work, so replacing with gn_project_graph and project_gdf, taken from OSMNX with a few changes to make compatible with GOSTnets

--- GOSTnets/core.py
+++ GOSTnets/core.py
@@ -2645,8 +2645,114 @@ def new_edge_generator(passed_geom, infra_type, iterator, existing_legitimate_po
 
     return nodes_to_add, edges_to_add, new_node_dict_entries, iterator
 
+
+def project_gdf(gdf, to_crs=None, to_latlong=False):
+    """
+    Taken from OSMNX
+
+    Project a GeoDataFrame from its current CRS to another.
+    If to_crs is None, project to the UTM CRS for the UTM zone in which the
+    GeoDataFrame's centroid lies. Otherwise project to the CRS defined by
+    to_crs. The simple UTM zone calculation in this function works well for
+    most latitudes, but may not work for some extreme northern locations like
+    Svalbard or far northern Norway.
+    Parameters
+    ----------
+    gdf : geopandas.GeoDataFrame
+        the GeoDataFrame to be projected
+    to_crs : string or pyproj.CRS
+        if None, project to UTM zone in which gdf's centroid lies, otherwise
+        project to this CRS
+    to_latlong : bool
+        if True, project to settings.default_crs and ignore to_crs
+    Returns
+    -------
+    gdf_proj : geopandas.GeoDataFrame
+        the projected GeoDataFrame
+    """
+    if gdf.crs is None or len(gdf) < 1:
+        raise ValueError("GeoDataFrame must have a valid CRS and cannot be empty")
+
+    # if to_latlong is True, project the gdf to latlong
+    if to_latlong:
+        gdf_proj = gdf.to_crs(settings.default_crs)
+        #utils.log(f"Projected GeoDataFrame to {settings.default_crs}")
+
+    # else if to_crs was passed-in, project gdf to this CRS
+    elif to_crs is not None:
+        gdf_proj = gdf.to_crs(to_crs)
+        #utils.log(f"Projected GeoDataFrame to {to_crs}")
+
+    # otherwise, automatically project the gdf to UTM
+    else:
+        if CRS.from_user_input(gdf.crs).is_projected:
+            raise ValueError("Geometry must be unprojected to calculate UTM zone")
+
+        # calculate longitude of centroid of union of all geometries in gdf
+        avg_lng = gdf["geometry"].unary_union.centroid.x
+
+        # calculate UTM zone from avg longitude to define CRS to project to
+        utm_zone = math.floor((avg_lng + 180) / 6) + 1
+        utm_crs = f"+proj=utm +zone={utm_zone} +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+
+        # project the GeoDataFrame to the UTM CRS
+        gdf_proj = gdf.to_crs(utm_crs)
+        #utils.log(f"Projected GeoDataFrame to {gdf_proj.crs}")
+
+    return gdf_proj
+
+
+def gn_project_graph(G, to_crs=None):
+    """
+    Taken from OSMNX. Project graph from its current CRS to another.
+    If to_crs is None, project the graph to the UTM CRS for the UTM zone in
+    which the graph's centroid lies. Otherwise, project the graph to the CRS
+    defined by to_crs.
+    Parameters
+    ----------
+    G : networkx.MultiDiGraph
+        the graph to be projected
+    to_crs : string or pyproj.CRS
+        if None, project graph to UTM zone in which graph centroid lies,
+        otherwise project graph to this CRS
+    Returns
+    -------
+    G_proj : networkx.MultiDiGraph
+        the projected graph
+    """
+    # STEP 1: PROJECT THE NODES
+    gdf_nodes = ox.utils_graph.graph_to_gdfs(G, edges=False)
+
+    # create new lat/lng columns to preserve lat/lng for later reference if
+    # cols do not already exist (ie, don't overwrite in later re-projections)
+    # if "lon" not in gdf_nodes.columns or "lat" not in gdf_nodes.columns:
+    #     gdf_nodes["lon"] = gdf_nodes["x"]
+    #     gdf_nodes["lat"] = gdf_nodes["y"]
+
+    # project the nodes GeoDataFrame and extract the projected x/y values
+    gdf_nodes_proj = project_gdf(gdf_nodes, to_crs=to_crs)
+    gdf_nodes_proj["x"] = gdf_nodes_proj["geometry"].x
+    gdf_nodes_proj["y"] = gdf_nodes_proj["geometry"].y
+    gdf_nodes_proj = gdf_nodes_proj.drop(columns=["geometry"])
+
+    # STEP 2: PROJECT THE EDGES
+    gdf_edges_proj = ox.utils_graph.graph_to_gdfs(G, nodes=False, fill_edge_geometry=False).drop(
+            columns=["geometry"]
+        )
+
+    # STEP 3: REBUILD GRAPH
+    # turn projected node/edge gdfs into a graph and update its CRS attribute
+    G_proj = ox.utils_graph.graph_from_gdfs(gdf_nodes_proj, gdf_edges_proj, G.graph)
+    #G_proj.graph["crs"] = gdf_nodes_proj.crs
+
+    #utils.log(f"Projected graph with {len(G)} nodes and {len(G.edges)} edges")
+    return G_proj
+
+
 def reproject_graph(input_net, source_crs, target_crs):
     """
+    to-do: delete, is not working
+
     Converts the node coordinates of a graph. Assumes that there are straight lines between the start and end nodes.
 
     :param input_net: a base network object (nx.MultiDiGraph)
@@ -2661,8 +2767,8 @@ def reproject_graph(input_net, source_crs, target_crs):
     #             pyproj.Proj(init=target_crs))
 
     # pyproj >= 2.1.0
-    wgs84 = pyproj.CRS(source)
-    utm = pyproj.CRS(target)
+    wgs84 = pyproj.CRS(source_crs)
+    utm = pyproj.CRS(target_crs)
     project_WGS_UTM = pyproj.Transformer.from_crs(wgs84, utm, always_xy=True).transform
 
     i = list(input_net.nodes(data = True))
@@ -2673,7 +2779,7 @@ def reproject_graph(input_net, source_crs, target_crs):
         #print(transform(project_WGS_UTM,j[1]['geom']))
         j[1]['x'] = transform(project_WGS_UTM,j[1]['geom']).x
         j[1]['y'] = transform(project_WGS_UTM,j[1]['geom']).y
-        j[1]['geom'] = transform(project_WGS_UTM,j[1]['geom'])
+        #j[1]['geom'] = transform(project_WGS_UTM,j[1]['geom'])
 
 
     return input_net
