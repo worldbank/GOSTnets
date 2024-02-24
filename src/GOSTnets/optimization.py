65b8defbfb4d9f8729af1e549d51dc31e314cd4d
elbeejay <jayaram.hariharan@utexas.edu>
2024-02-24 08:48:51 -0500

    doc-strings optimization.py

--- src/GOSTnets/optimization.py
+++ src/GOSTnets/optimization.py
@@ -28,15 +28,29 @@ def optimize_facility_locations(
     """
     Function for identifying spatially optimal locations of facilities (P-median problem)
 
-    :param OD: an Origin:Destination matrix, origins as rows, destinations as columns, in pandas DataFrame format.
-    :param facilities: The 'destinations' of the OD-Matrix. MUST be a list of objects included in OD.columns (or subset) if certain nodes are unsuitable for facility locations
-    :param p: the number of facilities to solve for
-    :param existing_facilities: facilities to always include in the solution. MUST be in 'facilities' list
-    :param verbose: print a bunch of status updates
-    :param execute: should the problem be executed
-    :param write: outPath to write problem
-    """
+    Parameters
+    ----------
+    OD : pd.DataFrame
+        an Origin:Destination matrix, origins as rows, destinations as columns, in pandas DataFrame format.
+    facilities : list
+        The 'destinations' of the OD-Matrix. MUST be a list of objects included in OD.columns (or subset) if certain nodes are unsuitable for facility locations
+    p : int
+        the number of facilities to solve for
+    existing_facilities : list
+        facilities to always include in the solution. MUST be in 'facilities' list
+    verbose : bool
+        print a bunch of status updates
+    execute : bool
+        should the problem be executed
+    write : str
+        outPath to write problem
 
+    Returns
+    -------
+    ans : list
+        a list of the optimal facility locations
+
+    """
     num_procs = mp.cpu_count()
     if verbose:
         print("cpu count: %s" % num_procs)
@@ -107,14 +121,24 @@ def optimize_facility_locations(
 
 
 def optimize_set_coverage(OD, max_coverage=2000, existing_facilities=None):
-    ### Determine the minimum number of facilities and their locations in order to cover all demands within a pre-specified maximum distance (or time) coverage (Location Set-Covering Problem) ###
-    # REQUIRED:   OD - an Origin:Destination matrix, origins as rows, destinations
-    #             as columns, in pandas DataFrame format.
-    #             max_coverage - The pre-specified maximum distance (or time) coverage.
-    # OPTIONAL:   existing_facilities - facilities to always include in the
-    #             solution. MUST be in 'facilities' list
-    # -------------------------------------------------------------------------#
+    """
+    Determine the minimum number of facilities and their locations in order to cover all demands within a pre-specified maximum distance (or time) coverage (Location Set-Covering Problem).
 
+    Parameters
+    ----------
+    OD : pd.DataFrame
+        An Origin:Destination matrix, origins as rows, destinations as columns, in pandas DataFrame format.
+    max_coverage : int
+        The pre-specified maximum distance (or time) coverage.
+    existing_facilities : list
+        Facilities to always include in the solution. Must be in 'facilities' list.
+
+    Returns
+    -------
+    list
+        A list of facility locations that provide coverage to all demands within the maximum coverage distance.
+
+    """
     # OD keys must be integers
     OD.columns = OD.columns.astype(int)
 
@@ -185,16 +209,28 @@ def optimize_partial_set_coverage(
     origins_pop_series=None,
     existing_facilities=None,
 ):
-    ### Function to determine the minimum number of facilities and their locations in order to cover a given fraction of the population within a pre-specified maximum distance (or time) coverage (Partial Set-Covering Problem). Do not use a demand-weighted OD matrix as an input. ###
-    # REQUIRED:   OD - an Origin:Destination matrix, origins as rows, destinations
-    #             as columns, in pandas DataFrame format.
-    #             max_coverage - The pre-specified maximum distance (or time) coverage.
-    #             pop_coverage - The given fraction of the population that should be covered
-    # OPTIONAL:   origins_pop_series - a series that contains each origin as the key, and each origin's population as the value
-    #             existing_facilities - facilities to always include in the
-    #             solution. MUST be in 'facilities' list
-    # -------------------------------------------------------------------------#
+    """
+    Function to determine the minimum number of facilities and their locations in order to cover a given fraction of the population within a pre-specified maximum distance (or time) coverage (Partial Set-Covering Problem). Do not use a demand-weighted OD matrix as an input.
 
+    Parameters
+    ----------
+    OD : pandas.DataFrame
+        An Origin:Destination matrix, origins as rows, destinations as columns.
+    pop_coverage : float, optional
+        The given fraction of the population that should be covered. Defaults to 0.8.
+    max_coverage : int, optional
+        The pre-specified maximum distance (or time) coverage. Defaults to 2000.
+    origins_pop_series : pandas.Series, optional
+        A series that contains each origin as the key, and each origin's population as the value. Defaults to None.
+    existing_facilities : list, optional
+        Facilities to always include in the solution. Defaults to None.
+
+    Returns
+    -------
+    list
+        A list of facility locations that cover the specified fraction of the population.
+
+    """
     # OD keys must be integers
     OD.columns = OD.columns.astype(int)
 
@@ -276,15 +312,28 @@ def optimize_max_coverage(
     origins_pop_series=None,
     existing_facilities=None,
 ):
-    ### Determine the location of P facilities in order to maximize the demand covered within a pre-specified maximum distance coverage (Max Cover). Do not use a demand-weighted OD matrix as an input. ###
-    # REQUIRED:   OD - an Origin:Destination matrix, origins as rows, destinations
-    #             as columns, in pandas DataFrame format.
-    #             max_coverage - The pre-specified maximum distance (or time) coverage.
-    #             p_facilities - The number of facilities to locate.
-    # OPTIONAL:   existing_facilities - facilities to always include in the
-    #             solution. MUST be in 'facilities' list
-    # -------------------------------------------------------------------------#
+    """
+    Determine the location of P facilities in order to maximize the demand covered within a pre-specified maximum distance coverage (Max Cover).
 
+    Parameters
+    ----------
+    OD : pandas.DataFrame
+        An Origin:Destination matrix, origins as rows, destinations as columns.
+    p_facilities : int
+        The number of facilities to locate. Default is 5.
+    max_coverage : int
+        The pre-specified maximum distance (or time) coverage. Default is 2000.
+    origins_pop_series : pandas.Series, optional
+        Series containing population data for each origin. Default is None.
+    existing_facilities : list, optional
+        List of facilities to always include in the solution. Default is None.
+
+    Returns
+    -------
+    list
+        List of facility locations that maximize the demand covered.
+
+    """
     # OD keys must be integers
     OD.columns = OD.columns.astype(int)
 
